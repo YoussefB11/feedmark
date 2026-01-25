dd4010d6fab852e1c551602723cb8cf3dc7a0429
YoussefB11 <y.baya-2021@hull.ac.uk>
2026-01-25 02:52:46 +0000

    Created a logo and added it and made some subtle ui changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/Logo_Feedmark.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">FeedMark</Link>
+        <Link to="/">
+        <img src={logo} alt="FeedMark" className="navbar-logo" />
+        </Link>
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Dashboard</Link></li>
